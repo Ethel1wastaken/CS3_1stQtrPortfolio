bb632dd11a8c758eca9479dd5a8d74cd17f321a9
Ethel1wastaken <b2029egtgamis@pshs.edu.ph>
2025-08-07 10:15:06 +0800

    Buldak Calculator

--- public/script.js
+++ public/script.js
@@ -1,8 +1,3 @@
-let x;
-
-x = 100;
-
-
-window.alert("Hello \nWorld.");
-document.write("Hello <br/>World.")
-console.log("Hello \nWorld.")
+let favSnack = "Buldak", age = 14, maxAge = 100, numPerDay = 2;
+let totalRequired = (numPerDay*365)*(maxAge-age);
+document.write(`You will need ${totalRequired} ${favSnack}s to last you until the ripe old age of ${maxAge}.`);
