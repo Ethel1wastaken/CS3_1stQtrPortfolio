95ccfa35a2d7bb4f8ec2964513fc4c6399f632e1
Ethel1wastaken <b2029egtgamis@pshs.edu.ph>
2025-08-07 10:24:26 +0800

    Buldak Calculator V2

--- public/script.js
+++ public/script.js
@@ -1,3 +1,12 @@
-let favSnack = "Buldak", age = 14, maxAge = 100, numPerDay = 2;
+let favSnack, numPerDay, age, maxAge;
+
+favSnack = window.prompt("What is your favorite snack?");
+numPerDay = window.prompt(`How many ${favSnack}s do you eat per day?`);
+age = window.prompt("How old are you?");
+maxAge = window.prompt("How old will you live to be? \nIf unknown, please consult a seer and come back later.");
+
 let totalRequired = (numPerDay*365)*(maxAge-age);
-document.write(`You will need ${totalRequired} ${favSnack}s to last you until the ripe old age of ${maxAge}.`);
+if(totalRequired<0)
+    document.write("Huh?");
+else
+    document.write(`You will need ${totalRequired} ${favSnack}s to last you until the ripe old age of ${maxAge}.`);
